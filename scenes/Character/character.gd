e0f5adbfd2086c05ee02696d47b73bcbd82c6b7d
Jdio02 <Jzukoski05@gmail.com>
2024-10-04 16:51:27 -0400

    Switched controls to WASD

--- scenes/Character/character.gd
+++ scenes/Character/character.gd
@@ -6,8 +6,8 @@ extends CharacterBody2D
 @onready var state = $StateMachine
 
 func _physics_process(_delta):
-	velocity.x = Input.get_axis("ui_left", "ui_right") * speed
-	velocity.y = Input.get_axis("ui_up", "ui_down") * speed
+	velocity.x = Input.get_axis("move_left", "move_right") * speed
+	velocity.y = Input.get_axis("move_up", "move_down") * speed
 
 	state.update_state(velocity)
 	
